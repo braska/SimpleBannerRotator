ef279be1f45e60003c7fa3f7e2f6e3082506a8b5
atnartur <atnartur2012@gmail.com>
2015-08-28 23:09:14 +0300

    + API запрос для получения банера для мобильной версии

--- app/controllers/RotatorController.php
+++ app/controllers/RotatorController.php
@@ -83,6 +83,66 @@ class RotatorController extends ControllerBase {
         $this->response->setContentType('text/javascript');
         $this->view->pick('rotator/js');
     }
+    
+    
+    public function get_mobileAction(){
+        $zone_id = $this->request->getQuery('zone_id', 'int');
+        $key = $this->request->getQuery(sha1($_SERVER['HTTP_HOST']), 'string');
+        
+        if(!$zone_id OR $key !== base64_encode(sha1(sha1($_SERVER['HTTP_HOST'])))){
+            $this->response->setStatusCode(400, 'Bad request');
+            return $this->response->send();
+        }
+
+        $banners = $this->modelsManager->createBuilder()
+            ->from(array('b'=>'App\Models\Banners'))
+            ->leftJoin('App\Models\Views', 'b.id = v.banner_id AND IF(b.start_date IS NULL, 1, IF(v.date >= b.start_date, 1, 0)) = 1 AND IF(b.end_date IS NULL, 1, IF(v.date < b.end_date, 1, 0)) = 1', 'v')
+            ->innerJoin('App\Models\BannersZones', 'b.id = bz.banner_id AND bz.zone_id = ' . $this->request->getQuery('zone_id', 'int'), 'bz')
+            ->andWhere('(end_date IS NULL OR end_date > ' . time() . ") AND (start_date IS NULL OR start_date <= " . time() . ") AND active = 1 AND archived = 0 AND type <> \"flash\" ")
+            ->groupBy('b.id')
+            ->having('max_impressions IS NULL OR COUNT(v.id) < max_impressions')
+            ->getQuery()
+            ->execute();
+            
+        if(count($banners)) {
+            $existsNonzeroPriority = false;
+            
+            if($banner->priority != 0)
+                $existsNonzeroPriority = true;
+
+            $segments = array();
+            $end = 0;
+            foreach ($banners as $banner) {
+                $priority = $existsNonzeroPriority ? $banner->priority : 1;
+                $segments[] = ['banner'=>$banner, 'start'=>$end, 'end'=>$end + $priority];
+                $end += $priority;
+            }
+
+
+            $rand = rand(0, $end * 100) / 100;
+            foreach($segments as $segment) {
+                if($segment['start'] <= $rand && $segment['end'] > $rand) {
+                    $banner_selected = $segment['banner'];
+                    break;
+                }
+            }
+
+            if((isset($banner_selected) && empty($banner_selected->id)) || !isset($banner_selected)) return;
+
+            $view = new Views();
+            $view->save(array('date'=>time(), 'banner_id'=>$banner_selected->id, 'zone_id'=>$this->request->getQuery('zone_id', 'int')));
+            $this->view->view = $view->id;
+
+            if($banner_selected->type == "image")
+                $this->view->pick('rotator/image');
+            else if($banner_selected->type == "html") {
+                $this->view->setRenderLevel(View::LEVEL_NO_RENDER);
+                echo $banner_selected->content;
+                return;
+            }
+            $this->view->banner = $banner_selected;
+        } else return;
+    }
 
     public function clickAction() {
         $id = $this->dispatcher->getParam('id');
